4d1cefbfd2b3f88fa6a09f42cda0d841d72d25c4
Broddiccus <Brody_man02@hotmail.com>
2019-11-17 15:39:08 -0500

    No More Crashing!
    all that's left is implimentation.

--- Source/core/src/com/mygdx/game/screen/SongSelection.java
+++ Source/core/src/com/mygdx/game/screen/SongSelection.java
@@ -2,6 +2,7 @@ package com.mygdx.game.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.mygdx.game.base.ScreenBeta;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -10,40 +11,44 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public final class SongSelection extends ScreenBeta {
     Skin Quantum;
-    String[] songNames = {"Cyberjazz Nights", "Neon Trip", "Cry No More"};
-    Button[] normalDifficulty = new Button[3];
-    Button[] expertDifficulty = new Button[3];
-    Button[] songTitle = new Button[3];
-    Button[][] songHolder = {songTitle, normalDifficulty, expertDifficulty};
+    String[] songNames;
+    TextButton[] normalDifficulty;
+    TextButton[] expertDifficulty;
+    TextButton[] songTitle;
+    TextButton[][] songHolder;
     @Override
     public void initialize() {
         Quantum = new Skin(Gdx.files.internal("Skin/Quantum UI SCF.json"));
+        songNames = new String[]{"Cyberjazz Nights", "Neon Trip", "Cry No More"};
+        normalDifficulty = new TextButton[3];
+        expertDifficulty = new TextButton[3];
+        songTitle = new TextButton[3];
+        songHolder = new TextButton[3][];
         for (int i = 0; i < songHolder.length; i++){
-            songTitle[i] = new Button(Quantum);
-            songTitle[i].setStyle(Quantum.get("default",Button.ButtonStyle.class));
+            songTitle[i] = new TextButton(songNames[i],Quantum);
+            songTitle[i].setStyle(Quantum.get("default",TextButton.TextButtonStyle.class));
             songTitle[i].setPosition((Gdx.graphics.getWidth()/2),((Gdx.graphics.getHeight()/2)-200)+(200*i), Align.center);
-            songTitle[i].setSize(300,100);
+            songTitle[i].setSize(400,200);
             songTitle[i].addListener(new ChangeListener() {
                 @Override
                 public void changed (ChangeEvent event, Actor actor) {
 
                 }
             });
-            st.addActor(normalDifficulty[i]);
-            normalDifficulty[i] = new Button(Quantum);
-            normalDifficulty[i].setStyle(Quantum.get("default",Button.ButtonStyle.class));
+            st.addActor(songTitle[i]);
+            normalDifficulty[i] = new TextButton("Normal",Quantum);
+            normalDifficulty[i].setStyle(Quantum.get("default",TextButton.TextButtonStyle.class));
             normalDifficulty[i].setPosition((Gdx.graphics.getWidth()/2)-200,((Gdx.graphics.getHeight()/2)-100)+(200*i), Align.center);
-            normalDifficulty[i].setSize(150,50);
+            normalDifficulty[i].setSize(200,75);
             normalDifficulty[i].addListener(new ChangeListener() {
                 @Override
                 public void changed (ChangeEvent event, Actor actor) {
 
                 }
             });
-            st.addActor(expertDifficulty[i]);
-            st.addActor(expertDifficulty[i]);
-            expertDifficulty[i] = new Button(Quantum);
-            expertDifficulty[i].setStyle(Quantum.get("default",Button.ButtonStyle.class));
+            st.addActor(normalDifficulty[i]);
+            expertDifficulty[i] = new TextButton("Expert",Quantum);
+            expertDifficulty[i].setStyle(Quantum.get("default",TextButton.TextButtonStyle.class));
             expertDifficulty[i].setPosition((Gdx.graphics.getWidth()/2)+200,((Gdx.graphics.getHeight()/2)-100)+(200*i), Align.center);
             expertDifficulty[i].setSize(150,50);
             expertDifficulty[i].addListener(new ChangeListener() {
@@ -55,6 +60,9 @@ public final class SongSelection extends ScreenBeta {
             st.addActor(expertDifficulty[i]);
 
         }
+        songHolder[0] = songTitle;
+        songHolder[1] = normalDifficulty;
+        songHolder[2] = expertDifficulty;
     }
 
     @Override
